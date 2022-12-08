c4d7e99da2cef0d74ac05307340e0f5dc91e8e43
Lura.Misner <lmisner@uvm.edu>
2022-12-08 14:46:02 -0500

    Basic functionality of whac-a-mole

--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -3,6 +3,21 @@ const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+let countDownTimerId = setInterval(countDown, 1000)
+
+// Check if the mole has been clicked on to increment the score
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
 
 function randomSquare() {
     // Remove mole if it exists
@@ -13,6 +28,26 @@ function randomSquare() {
     // Pick a random square to make the mole
     let randomS = squares[Math.floor(Math.random() * squares.length)]
     randomS.classList.add('mole')
+    hitPosition = randomS.id
 }
 
-randomSquare()
+function moveMole() {
+    // Pops up a mole at random
+    timerId = setInterval(randomSquare, 500)
+}
+
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent=currentTime
+
+    // Handle what happens when timer hits 0
+    if (currentTime == 0){
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is ' + result)
+    }
+
+}
+
+moveMole()
